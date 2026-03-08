8be46fb92c81974984a4294394f0396c98530d1c
bardiel73 <76916600+bardiel73@users.noreply.github.com>
2026-03-08 19:36:32 +0300

    I feel like this is better but it's still broken

--- window_nostd.c
+++ window_nostd.c
@@ -3,28 +3,39 @@
 #define BARDLIB_IMPLEMENTATION
 #include "bardlib.h"
 
-bool on_line(int64_t idx, int64_t x1, int64_t y1, int64_t x2, int64_t y2)
-{
-    double slope = 0;
+#define BARD_ABS(x) (x) <= 0 ? -(x) : (x)
 
-    if (x1 == x2) {
-        slope = 1;
+bool idx_is_on_line(uint64_t idx, uint64_t x1, uint64_t y1, uint64_t x2, uint64_t y2)
+{
+    if (idx >= (global_state.window_height * global_state.window_width)) {printf_win32("WARNING: idx_is_on_line: idx too big \n)"); return false;}
+
+    uint64_t idx_x = (uint64_t)(idx % global_state.window_width);
+    uint64_t idx_y = (uint64_t)(idx / global_state.window_width);
+
+    bool satisfies_y = false;
+    bool satisfies_x = false;
+
+    if (y1 == y2) {
+        satisfies_y = idx_y == y1;
     } else {
-        slope = (double)(y1 - y2) / (double)(x1 - x2);
+        satisfies_y = y1 < y2 ? y1 <= idx_y && idx_y <= y2 : y2 <= idx_y && idx_y <= y1;
     }
 
-    // y = mx + b
-    // y = slope * x + b;
-    // b = (y1 - slope * x);
-    int64_t constant = y1 - (int64_t)(slope * x1);
-    int64_t idx_x = idx % global_state.window_width;
-    int64_t idx_y = idx / global_state.window_height;
-    bool satisfies_line = idx_y == (int64_t)((int64_t)(idx_x * slope) + constant);
+    if (x1 == x2) {
+        satisfies_x = idx_x == x1;
+    } else {
+        satisfies_x = x1 < x2 ? x1 <= idx_x && idx_x <= x2 : x2 <= idx_x && idx_x <= x1; 
+    }
 
-    bool between_points_x = x2 > x1 ? x1 <= idx_x && idx_x <= x2 : x2 <= idx_x && idx_x <= x1;
-    bool between_points_y = y2 > y1 ? y1 <= idx_y && idx_y <= y2 : y2 <= idx_y && idx_y <= y1;
+    // y = mx + n
+    // m = (y1 - y2) / (x1 - x2)
+    // n = y - mx
+    double slope = x1 == x2 ? 1 : (double)(y1 - y2) / (double)(x1 - x2); // TODO: handle x1 == x2 case
+    int64_t constant = y1 - slope * x1;
 
-    return satisfies_line && between_points_x && between_points_y;
+    bool satisfies_line = (idx_y == slope * idx_x + constant); // TODO: add tolerance level instead of exact match, thickness?
+
+    return satisfies_x && satisfies_y && satisfies_line;
 }
 
 void bard_draw_line(uint64_t x1, uint64_t y1, uint64_t x2, uint64_t y2, uint32_t color)
@@ -32,16 +43,16 @@ void bard_draw_line(uint64_t x1, uint64_t y1, uint64_t x2, uint64_t y2, uint32_t
     uint64_t idx1 = y1 * global_state.window_width + x1;
     uint64_t idx2 = y2 * global_state.window_width + x2;
 
-    if (idx1 == idx2) {global_state.pixels[idx1] = color; return;}
+    if (idx1 == idx2) {global_state.pixels[idx1] = color; printf_win32("WARNING: idx1 == idx2, coloring single pixel and exiting \n)"); return;}
 
     uint64_t biggeridx = idx1 > idx2 ? idx1 : idx2;
     uint64_t smalleridx = idx1 < idx2 ? idx1 : idx2;
 
-    if (biggeridx >= (global_state.window_height*global_state.window_width)) {return;}
+    if (biggeridx >= (global_state.window_height * global_state.window_width)) {printf_win32("WARNING: biggeridx bigger than window :( \n)"); return;}
 
     for (uint64_t idx = smalleridx; idx <= biggeridx; ++idx)
     {
-        if (on_line(idx, x1, y1, x2, y2)) {
+        if (idx_is_on_line(idx, x1, y1, x2, y2)) {
             global_state.pixels[idx] = color;
         }
     }
@@ -59,18 +70,16 @@ void bard_clear_background(uint32_t color)
 
 int user_main()
 {
-    printf_win32("is this flippin %s\n", "working");
-
-    return 0;
-    bard_create_window_win32(800, 600, "this is a title");
+    constexpr uint64_t factor = 60;
+    bard_create_window_win32(factor*16, factor*9, "this is a title");
 
     while (!global_state.window_should_close)
     {
         bard_begin_drawing_win32();
-        // bard_clear_background(BARD_RGB(255, 255, 255));
+        bard_clear_background(BARD_RGB(255, 255, 255));
 
         // TODO this is incorrect, it also breaks at 800 600 for another incorrectness
-        bard_draw_line(0, 0, 799, 599, BARD_RGB(255, 0, 0));
+        bard_draw_line(0, 0, factor*16 - 1, factor*9 - 1, BARD_RGB(255, 0, 0));
 
         bard_end_drawing_win32();
     }
